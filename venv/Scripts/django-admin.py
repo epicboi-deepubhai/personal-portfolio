31603542650077d904da5548b7fddab8f8ecdb5a
Deepanshu <deepanshumi200@gmail.com>
2020-11-10 00:55:44 +0530

    redid settings.py file

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Dee\PycharmProjects\personal-portfolio\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
